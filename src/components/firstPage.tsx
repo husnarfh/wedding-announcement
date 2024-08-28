2e0e4647fd5b83d2538abe52437f36a73db8d3c9
Husna Nurarifah <husnanrarifah@gmail.com>
2024-08-29 06:20:07 +0700

    change bg

--- src/components/firstPage.tsx
+++ src/components/firstPage.tsx
@@ -1,14 +1,9 @@
 'use client'
 
 import { useEffect, useState } from "react";
-import { Dancing_Script, Pinyon_Script, Montserrat } from 'next/font/google'
 import localFont from 'next/font/local'
 import '../styles/card.css'
 
-
-const ds = Dancing_Script({ subsets: ['latin'] })
-const ps = Pinyon_Script({ subsets: ['latin'], weight: '400' })
-const montserrat = Montserrat({ subsets: ['latin'], weight: ['400', '600'] })
 const peaches = localFont({ src: '../../public/peaches.ttf' })
 
 export default function FirstPage() {
@@ -91,7 +86,7 @@ export default function FirstPage() {
                             <div className="front page-1 w-screen text-center justify-between flex flex-col p-6">
                                 <p style={{ letterSpacing: '5px'}}>TAMI & SYARIF</p>
 
-                                <p className={peaches.className + ' text-7xl'}>We're <br></br> Getting <br></br> Married</p>
+                                <p className={peaches.className + ' text-7xl'}>We&apos;re <br></br> Getting <br></br> Married</p>
 
                                 <p style={{ letterSpacing: '5px'}}>28 AGUSTUS 2024</p>
                             </div>
@@ -112,13 +107,17 @@ export default function FirstPage() {
                 {
                     page === 1 && (
                         <article id="page-1">
-                            <div className={'front page-2 text-center p-6 w-screen'}>
+                            <div className={'front page-2 text-center p-6 w-screen content-center'}>
                                 <div className="text-center">
                                     <h1 className={peaches.className + ' text-7xl pb-3'}>We Got Married</h1>
-                                    <p className="py-6">Kami sangat berbahagia untuk mengumumkan bahwa kami telah menjadi sepasang suami istri dalam acara pernikahan yang intim dan dikelilingi oleh keluarga dan orang-orang terdekat.</p>
-                                    <p>Kami sangat bersyukur atas cinta dan dukungan yang telah diberikan oleh keluarga dan teman-teman.</p>
-
-                                    <div className="py-20">
+                                    <p className="py-5">Dengan segala kesederhaan dan dikelilingi keluarga dan sahabat terdekat kami. 
+                                    Telah berlangsung pernikahan kami <br /> pada <b>28 Agustus 2024</b> di <b>Haurngombong, Pamulihan, Sumedang</b>. <br />
+                                    Terimakasih atas segenap doa dan dukungannya untuk kami
+                                    </p>
+                                    <p style={{
+                                        fontFamily: 'monospace', fontStyle: 'italic'
+                                    }} className="mt-3">"A great marriage isn't something that just happens; it's something that must be created." - Fawn Weaver</p>
+                                    <div className="py-16">
                                         <p className="">Yang berbahagia</p>
                                         <p className="" style={{ letterSpacing: '4px'}}>TAMI & SYARIF</p>
                                     </div>
@@ -210,7 +209,7 @@ export default function FirstPage() {
                         <article id="page-4">
                             <div className="front page-3 p-6 w-screen justify-between flex flex-col">
                                 <p className="text-left">OUR GALLERY</p>
-                                <p className="text-right">#hastag</p>
+                                <p className="text-right mb-5">#hastag</p>
 
                                 <div className="absolute bottom-0 left-0 text-black" onClick={goPrevPage}>
                                 <img
